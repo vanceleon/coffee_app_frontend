bb410cc0207368dd2c89c3c5ecf2ad1909b80c1f
vanceleon <vanceleon44@gmail.com>
2020-05-04 17:02:13 -0400

    adding different coffee images building out measurements component

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import CoffeeLandingImg from '../images/coffeeLanding.jpg'
+import CoffeeLandingImg from '../images/coffeeBackground.jpg'
 
 export default function LandingPage() {
   return (
     <div className='landing-page'>
-      <h1>Welcome to Home Barista App</h1>
-      <img src={CoffeeLandingImg} alt="latte" height='500px' width='400px'/>
+      <h1>Home Barista</h1>
+      <img src={CoffeeLandingImg} alt="latte" height='1280px' width='100%'/>
       <div>Log In</div>
       <div>Sign In</div>
     </div>
--- src/components/measurements.js
+++ src/components/measurements.js
@@ -7,14 +7,21 @@
 // rating on results
 
 import React, {useState} from 'react';
+import Rating from './ratings';
 
 
-export default function CoffeeType() {
+export default function CoffeeMeasurements() {
   const [coffee, setcoffee] = useState(initialState)
   return(
-    <div>
-      <input type="text" placeholder='input coffee here'/>
+    <form>
+      <input type="text" placeholder='coffee type'/>
+      <input type="text" placeholder='timing'/>
+      <input type="text" placeholder='weight'/>
+      <input type="text" placeholder='water temperature'/>
+      <input type="text" placeholder='milk temperature'/>
+      <Rating/>
+
       <div className="submit">Submit</div>
-    </div>
+    </form>
   )
 }
