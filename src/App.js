901b129046f4b2c1d61e9a6b04307b3c45a593ab
vanceleon <vanceleon44@gmail.com>
2020-05-09 20:14:29 -0400

    edited file structure for css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './css/App.css';
 import LandingPage from './components/landingPage';
 import Home from './components/home';
 // import NewBrewForm from './components/newForm';
