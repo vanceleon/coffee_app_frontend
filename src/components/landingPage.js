baa440278166eb7b755528cab457301ec57f7b47
vanceleon <vanceleon44@gmail.com>
2020-05-26 23:22:36 -0400

    notes

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -3,10 +3,11 @@ import CoffeeLandingImg from '../images/coffeeBackground.jpg'
 
 
 export default function LandingPage() {
+  //need to change the img tag to a div tag and set the background to the image
   return (
     <div className='landingPage'>
       <h1>Home Barista</h1>
-      <img className='landingImg' src={CoffeeLandingImg} alt="latte"/>
+      <img className='landingImg' src={CoffeeLandingImg} alt="latte"/> 
       <div>Log In</div>
       <div>Sign In</div>
       <div>Sign Up</div>
