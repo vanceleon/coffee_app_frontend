f444be00a49a5140c7281affdc48db273365bed2
vanceleon <vanceleon44@gmail.com>
2020-05-30 23:52:26 -0400

    firing function

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -4,17 +4,18 @@ import CoffeeLandingImg from '../images/coffeeBackground.jpg'
 
 export default function LandingPage() {
   //need to change the img tag to a div tag and set the background to the image
-  isLoggedIn(signedIn) {
-    if(signedIn) {
-      return true;
-    }
+  const isLoggedIn = (signedIn) => {
+    console.log('on click for loginz')
+    // if(signedIn) {
+    //   // return true;
+    // }
   }
   return (
     <div className='landingPage'>
       <h1>Home Barista</h1>
       <img className='landingImg' src={CoffeeLandingImg} alt="latte"/> 
       <div>Log In</div>
-      <div>Sign In</div>
+      <div onClick={isLoggedIn}>Sign In</div>
       <div>Sign Up</div>
     </div>
   )
